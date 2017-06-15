85d9a99ceeaa5e04291c8f1da33da8b0e00411cf
Rafael <rafaelbecks93@gmail.com>
2017-06-15 14:29:33 -0430

    agreados métodos de conexión e inserción de datos en redis

--- index.php
+++ index.php
@@ -1,18 +1,60 @@
 <?php
 
+//Dependencias
+//sudo apt-get install lamp-server^ 
+//sudo apt-get install php5-redis
+
 require 'vendor/autoload.php';
 
 $app = new \Slim\Slim;
+
+$app->response->headers->set('Content-Type', 'application/json');
+
+$redis = new Redis();
+$connection = $redis->connect("127.0.0.1", "6379");
 
 $app->get('/', function () {
     echo "Redis Client API";
 });
 
-$app->get('/redis', function () {
-    $redis = new Redis();
-    $connection = $redis->connect("127.0.0.1", "6379");
+$app->get('/verificacion-conexion', function () use($connection) {
+    try {
+        if($connection)
+            $response = array("mensaje" => "La conexión a redis ha sido configurada exitosamente");
+        else
+            $response = array("mensaje" => "La conexión a redis no ha podido establecerse");
+
+        echo json_encode($response);
+
+    } catch (Exception $e) {
+        $response = array("mensaje" => "Ha ocurrido un error al intentar conectarse a Redis: ".$e->getMessage());
+
+        echo json_encode($response);
+    }
 });
 
+$app->post('/agregar-string/:key',function($key) use($app,$redis){
+
+    $body = json_decode($app->request->getBody());
+
+    try {
+        foreach ($body->value as $member) {
+            $redis->sAdd($key,$member);        
+        }
+
+        $response = array("mensaje" => "El set ".$key." ha sido actualizado exitosamente");
+
+        echo json_encode($response);
+        
+    } catch (Exception $e) {
+        $response = array("mensaje" => "Ha ocurrido un error al intentar conectarse a Redis: ".$e->getMessage());
+
+        echo json_encode($response);
+    }
+
+});
+
+    // $result = $redis->sAdd("test" , "test otro"); 
 
 $app->run();
 
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit89af9d3ff4024bf64648b1983e0bc9ce
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit89af9d3ff4024bf64648b1983e0bc9ce', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
