e652260dfcc55454d79ff392ec14172f119af80f
Rafael <rafaelbecks93@gmail.com>
2017-06-16 12:22:36 -0430

    agregados nuevos metodos de acceso a datos redis

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 
-//Dependencias
-//sudo apt-get install lamp-server^ 
-//sudo apt-get install php5-redis
-
 require 'vendor/autoload.php';
 
 $app = new \Slim\Slim;
@@ -45,7 +41,7 @@ $app->post('/agregar-string/:key',function($key) use($app,$redis){
         $response = array("mensaje" => "El set ".$key." ha sido actualizado exitosamente");
 
         echo json_encode($response);
-        
+
     } catch (Exception $e) {
         $response = array("mensaje" => "Ha ocurrido un error al intentar conectarse a Redis: ".$e->getMessage());
 
@@ -54,7 +50,98 @@ $app->post('/agregar-string/:key',function($key) use($app,$redis){
 
 });
 
-    // $result = $redis->sAdd("test" , "test otro"); 
+$app->get("/listar-string/:key",function($key) use($app,$redis){
+
+    try {
+        $data = $redis->sMembers($key);
+
+        if(count($data) > 0)
+            echo json_encode(array("list" => $data));
+        else
+            echo json_encode(array("mensaje" => "No existen datos asociados al key"));
+        
+    } catch (Exception $e) {
+        echo $e->getMessage();        
+    }
+});
+
+$app->put("/remover-elemento/:key",function($key) use($app, $redis){
+
+    try {
+        $body = json_decode($app->request->getBody());
+
+        $redis->sRem($key,$body->value);
+
+        echo json_encode(array("mensaje" => "El elemento ".$body->value." ha sido removido del set"));
+    } catch (Exception $e) {
+        echo $e->getMessage();        
+    }
+
+});
+
+$app->delete("/eliminar/:key", function($key) use($redis){
+
+    try {
+        
+        $result = $redis->delete($key);
+
+        if($result)
+        {
+            echo json_encode(array("mensaje" => "El set ".$key." ha sido eliminado exitosamente"));
+        }
+
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+});
+
+$app->get("/verificar-existencia/:key/:valor",function($key,$valor) use($redis){
+
+    try {   
+
+        $result = $redis->sIsMember($key,$valor);
+
+        $respuesta = ($result) ? "El valor existe en el set" : "El valor no existe";
+
+        echo json_encode(array("mensaje" => $respuesta));
+
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+
+});
+
+$app->get("/interseccion/:seta/:setb",function($seta,$setb) use($redis){
+
+    try {
+        
+        $result = $redis->sInter($seta,$setb);
+
+        $respuesta = (count($result)>0) ? $result : "No existen elementos comunes entre los sets";
+
+        echo json_encode(array("mensaje" => $respuesta));
+
+    } catch (Exception $e) {
+        echo $e->getMessage();   
+    }
+
+});
+
+$app->put("/interseccion-multiple",function() use($app,$redis)
+{
+    try {
+        $body = json_decode($app->request->getBody());
+
+        $result = call_user_func_array(array($redis, "sInter"), $body->sets);   
+
+        $respuesta = (count($result)>0) ? $result : "No existen elementos comunes entre los sets";
+
+        echo json_encode(array("mensaje" => $respuesta));
+
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+});
 
 $app->run();
 
